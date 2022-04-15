a506c283d546e4fc007cf5bc71e7e98d9ed5e987
David Roy <24599987+DavidSRoy@users.noreply.github.com>
2022-04-14 17:11:43 -0700

    setting up messaging API

---
+++ API/functions/index.js
@@ -0,0 +1,34 @@
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+const express = require('express');
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+
+const firestore = admin.firestore();
+
+const PROJECTID = 'sociable-messenger';
+const USERS = firestore.collection('test_users');
+
+const messaging_api = express();
+
+
+async function getUsers() {
+    const snapshot = await USERS.get();
+    return snapshot.docs.map(doc => doc.data());
+    //.map(doc => doc.id);
+  }
+
+messaging_api.get('/test', async (request, response) => {
+    response.json(await getUsers());
+});
+
+
+
+
+exports.messaging_api = functions.https.onRequest(messaging_api)
