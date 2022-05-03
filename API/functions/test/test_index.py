66c7405f566a3684b9379a22e64029d19af0e322
David Roy <24599987+DavidSRoy@users.noreply.github.com>
2022-05-02 21:16:23 -0700

    added pytest configs

--- API/functions/test/test_index.py
+++ API/functions/test/test_index.py
@@ -5,7 +5,6 @@ import json
 BASE_URL = "https://us-central1-sociable-messenger.cloudfunctions.net/messaging_api/"
 #API = BASE_URL + "?q={city_name}&appid={api_key}&units=metric"
 API = ""
-API_KEY = "fa8183b09992df0844473872b19d64fa7c400842"
 
 class Test_Index:
     def test_messaging_getUsers_returns401WithoutAuth(self):
@@ -14,16 +13,9 @@ class Test_Index:
         res = requests.get(url, headers=headers)
         assert res.status_code == 401
 
-    def test_messaging_getUsers_returns401WithAuth(self):
+    def test_messaging_getUsers_returns401WithAuth(self, auth_token):
         url = BASE_URL + "getUsers"
-        headers = {'auth': get_api_key()}
+        headers = {'auth': auth_token}
+        print(auth_token)
         res = requests.get(url, headers=headers)
         assert res.status_code == 200
-
-def get_api_key():
-    try:
-        with open('../.runtimeconfig.json', 'r') as f:
-            res = json.load(f)
-            return res['messaging']['key']
-    except Exception:
-        return "null"
