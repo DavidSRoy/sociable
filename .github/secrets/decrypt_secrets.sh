a4d7cf69da1d20a50aba0eae3585ac96dfdc8a26
0xMango <99053377+0xMango@users.noreply.github.com>
2022-05-09 14:36:50 -0700

    Create decrypt_secrets.sh

---
+++ .github/secrets/decrypt_secrets.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -eo pipefail
+
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/Sociable.mobileprovision.mobileprovision ./.github/secrets/Sociable.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/distribution.cer ./.github/secrets/distribution.cer.gpg
+
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+
+cp ./.github/secrets/Sociable.mobileprovision.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/Sociable.mobileprovision.mobileprovision
+
+
+security create-keychain -p "" build.keychain
+security import ./.github/secrets/Certificates.cer -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+
+security list-keychains -s ~/Library/Keychains/build.keychain
+security default-keychain -s ~/Library/Keychains/build.keychain
+security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+
+security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
