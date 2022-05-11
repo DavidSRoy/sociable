db6185b3891e8b0771f22ea6bfdacb516270fd35
David Roy <24599987+DavidSRoy@users.noreply.github.com>
2022-05-10 23:28:53 -0500

    updated base url

--- API/functions/test/test_index.py
+++ API/functions/test/test_index.py
@@ -2,7 +2,8 @@ import requests
 import pytest
 import json
 
-BASE_URL = "http://localhost:5001/sociable-messenger/us-central1/messaging_api/"
+BASE_URL = "https://us-central1-sociable-messenger.cloudfunctions.net/messaging_api/"
+#BASE_URL = "http://localhost:5001/sociable-messenger/us-central1/messaging_api/"
 #API = BASE_URL + "?q={city_name}&appid={api_key}&units=metric"
 API = ""
 
