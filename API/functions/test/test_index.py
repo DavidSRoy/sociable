21f39585903a49e1ad32262f8d9ab0c09725670c
David Roy <24599987+DavidSRoy@users.noreply.github.com>
2022-05-10 23:48:25 -0500

    Feature/update python tests (#37)
    * initial updates
    
    * updated function test yml to run emulator for testing
    
    * update1
    
    * updated packages
    
    * added only functions flag
    
    * added ci token
    
    * updated tests to only include 401 checks
    
    * updated base url

--- API/functions/test/test_index.py
+++ API/functions/test/test_index.py
@@ -3,6 +3,7 @@ import pytest
 import json
 
 BASE_URL = "https://us-central1-sociable-messenger.cloudfunctions.net/messaging_api/"
+#BASE_URL = "http://localhost:5001/sociable-messenger/us-central1/messaging_api/"
 #API = BASE_URL + "?q={city_name}&appid={api_key}&units=metric"
 API = ""
 
@@ -13,23 +14,8 @@ class Test_Index:
         res = requests.get(url, headers=headers)
         assert res.status_code == 401
 
-    def test_messaging_getUsers_returns401WithAuth(self, auth_token):
-        url = BASE_URL + "getUsers"
-        headers = {'auth': auth_token}
-        print(auth_token)
-        res = requests.get(url, headers=headers)
-        assert res.status_code == 200
-
     def test_messaging_getMessages_returns401WithoutAuth(self):
         url = BASE_URL + "getMessages"
         headers = {}
         res = requests.get(url, headers=headers)
         assert res.status_code == 401
-
-    def test_messaging_getMessages_doesNotReturn401WithAuth(self, auth_token):
-        url = BASE_URL + "getMessages"
-        headers = {'auth': auth_token}
-        params = {'uid':'abcde'}
-        print(auth_token)
-        res = requests.get(url, headers=headers, params=params)
-        assert res.status_code != 401
