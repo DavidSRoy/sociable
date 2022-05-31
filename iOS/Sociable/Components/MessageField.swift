b3cd7e70a9c35f9750573c253c783688faac9536
0xMango <kevchoi@uw.edu>
2022-05-31 01:44:22 -0700

    Revert MessageField & ContactScreen

--- iOS/Sociable/Components/MessageField.swift
+++ iOS/Sociable/Components/MessageField.swift
@@ -9,14 +9,12 @@ import SwiftUI
 
 struct MessageField: View {
     @State private var message = ""
-    @Environment(\.colorScheme) var colorScheme
-    @EnvironmentObject var mm: MessageManager
+    
     var body: some View {
         HStack {
             SendFormm(tmp: Text("Type your message here"), text: $message)
         Button {
-            mm.sendMessage(msg: message, recipient: "jane1")
-            message = ""
+        print("Messaging not implemented yet")
         }
     label: {
             Image(systemName: "paperplane.fill")
@@ -25,18 +23,18 @@ struct MessageField: View {
                 .background(Color("msgblue"))
                 .cornerRadius(50)
         }
-        }
+      }
         .padding(.horizontal)
         .padding(.vertical, 10)
-        .background(colorScheme == .dark ? Color.black : Color("gray"))
+        .background(Color("gray"))
         .cornerRadius(50)
         .padding()
-    }
+   }
 }
 
 struct MessageField_Previews: PreviewProvider {
     static var previews: some View {
-        MessageField().environmentObject(MessageManager())
+        MessageField()
     }
 }
 
@@ -45,15 +43,15 @@ struct SendFormm: View {
     @Binding var text: String
     var editingChanged: (Bool) -> () = {_ in}
     var coms: () -> () = {}
-
+    
     var body: some View {
         ZStack (alignment: .leading) {
             if text.isEmpty {
                 tmp.opacity(0.5)
             }
-
+            
             TextField("", text: $text, onEditingChanged: editingChanged, onCommit: coms)
         }
     }
-
+    
 }
--- iOS/Sociable/ContactScreen.swift
+++ iOS/Sociable/ContactScreen.swift
@@ -8,40 +8,25 @@
 import SwiftUI
 
 struct ContactScreen: View {
-    @StateObject
-    private var cvm = ContactBubble()
     var profiles = ["https://ca.slack-edge.com/T039K0BN264-U038Y2U704A-0da3c7494a42-512" , "https://ca.slack-edge.com/T039K0BN264-U038Y2U704A-0da3c7494a42-512", "https://ca.slack-edge.com/T039K0BN264-U038Y2U704A-0da3c7494a42-512"];
     var names = ["David", "Sarah", "Marty"]
     var body: some View {
-        ContactTitleRow()
-        NavigationView {
-                List {
-                    ForEach(cvm.contacts) { contact in
-                        VStack(alignment: .leading) {
-                            HStack {
-                                Label(contact.fname, systemImage: "person.crop.circle")
-                                Text(contact.lname)
-                            }
-                            Divider()
-                            Label(contact.phone ?? "Not Found", systemImage: "apps.iphone")
-                        }
-                        .padding()
-                        .background(Color("msgblue"))
-                        .clipShape(RoundedRectangle(cornerRadius: 15.0, style: .continuous))
-                    }
+        VStack {
+        VStack {
+            ContactTitleRow()
+            ScrollView {
+                ForEach (names.indices) { value in
+                    Contact(imgURL: URL(string: profiles[value])!, name: names[value])
                 }
             }
-        .alert(item: $cvm.perme) {  h in
-            Alert(
-                title: Text("Permission Denied"),
-                message: Text(cvm.perme?.desc ?? "Unknown Error"),
-                dismissButton:
-                        .default(Text("OK"))
-                
-            )
+            .padding(.top, 10)
+            .background(.white)
         }
+        .background(Color("msgblue"))
         }
+        
     }
+ }
 
 struct ContactScreen_Previews: PreviewProvider {
     static var previews: some View {
